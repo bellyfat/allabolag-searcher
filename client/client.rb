24bc0d4a2a4c9589448153c8e313c49b83b84cdf
Hampus Liljekvist <hlilje@users.noreply.github.com>
2014-03-31 20:39:52 +0200

    Update client script

--- client/client.rb
+++ client/client.rb
@@ -11,20 +11,30 @@ module ClientModule
     def fetch(search)
       escaped = CGI.escape(search)
       url = "http://localhost:3000?q=#{escaped}"
-      response = JSON.parse(open(url, "Content-Type" => "application/json",
-                                 "Accept" => "application/json").read)
+
+      begin 
+        response = JSON.parse(open(url, "Content-Type" => "application/json",
+                                   "Accept" => "application/json").read)
+      rescue => e
+        if OpenURI::HTTPError
+          puts "Invalid search term"
+        else
+          raise e
+        end
+        abort
+      end
 
       reg_num = response[0]["reg_number"] 
       found = response[0]["found"]
 
-      return found == false ? "Organisation not found" : reg_num
+      return found ? reg_num : "Organisation not found"
     end
   end
 
   if __FILE__ == $PROGRAM_NAME
     client = ClientModule::Client.new
     if ARGV.length == 0
-       puts "Enter an organiation name as search argument"
+      puts "Enter an organiation name as search argument"
     else
       puts client.fetch(ARGV.join("+"))
     end
